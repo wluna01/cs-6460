8a16c501b4e4d154da3226256d6b70c8f6bbf2e1
Will Luna <wluna@anaconda.com>
2024-06-22 13:15:38 -0700

    added .env and auth

--- Final Project/Code/hello_world.py
+++ Final Project/Code/hello_world.py
@@ -1,4 +1,34 @@
 import streamlit as st
 import pandas as pd
+#from snowflake.snowpark.context import get_active_session
+import snowflake.connector
+from datetime import datetime, timedelta
+import nltk
+from nltk.tokenize import word_tokenize
+from dotenv import load_dotenv
+import os
 
-st.title("hello world")
+load_dotenv()
+
+snowflake_user = os.getenv('SNOWFLAKE_USER')
+snowflake_password = os.getenv('SNOWFLAKE_PASSWORD')
+snowflake_account = os.getenv('SNOWFLAKE_ACCOUNT')
+
+# Get the current credentials
+#session = get_active_session()
+conn = snowflake.connector.connect(
+    user=snowflake_user,
+    password=snowflake_password,
+    account=snowflake_account
+)
+
+# Write directly to the app
+st.title("La Tortuga Gigante")
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM educational_technology.stories.la_tortuga_gigante LIMIT 10")
+result = cursor.fetchall()
+cursor.close()
+conn.close()
+for row in result:
+    st.text(row)
